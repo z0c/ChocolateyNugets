461f979defc1afee33fa8a6c28f381c8b0e6db83
z0c <nvnivs@gmail.com>
2014-07-15 10:26:46 +0100

    Changed source for Sql Server 2008 R2 express to advanced installer to all choosing more features

--- SqlServer2008R2Express/tools/chocolateyInstall.ps1
+++ SqlServer2008R2Express/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'SqlServer2008R2Express'
 $installerType = 'EXE'
-$url = 'http://download.microsoft.com/download/0/4/B/04BE03CD-EAF3-4797-9D8D-2E08E316C998/SQLEXPR_x86_ENU.exe'
-$url64 = 'http://download.microsoft.com/download/0/4/B/04BE03CD-EAF3-4797-9D8D-2E08E316C998/SQLEXPR_x64_ENU.exe'
+$url = 'http://download.microsoft.com/download/0/4/B/04BE03CD-EAF3-4797-9D8D-2E08E316C998/SQLEXPRADV_x86_ENU.exe'
+$url64 = 'http://download.microsoft.com/download/0/4/B/04BE03CD-EAF3-4797-9D8D-2E08E316C998/SQLEXPRADV_x64_ENU.exe'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
