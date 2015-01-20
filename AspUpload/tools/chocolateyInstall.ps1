831243c02de6b24ae1586846b6bebae672e4e131
z0c <nvnivs@gmail.com>
2015-01-20 10:53:31 +0000

    Set download Url to owner site to comply with licence

--- AspUpload/tools/chocolateyInstall.ps1
+++ AspUpload/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = 'AspUpload'
 $installerType = 'EXE'
-$url = 'https://github.com/z0c/ChocolateyNugets/blob/master/AspUpload/AspUpload.3.1.0.2.exe?raw=true'
-$url64 = 'https://github.com/z0c/ChocolateyNugets/blob/master/AspUpload/AspUpload64.3.1.0.2.exe?raw=true'
+$url = 'http://www.persits.com/aspupload.exe'
+$url64 = 'http://www.persits.com/aspupload64.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
